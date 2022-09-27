98581bf8b1e4813e53f14ab74d46dae4fbed6973
Tobias Lindström <me@tobiaslindstrom.se>
2022-09-27 16:56:41 +0200

    Trying to read e2e.toml and zip the files.

--- index.js
+++ index.js
@@ -1,21 +1,51 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+const core = require('@actions/core')
+const http = require('@actions/http-client')
+const exec = require('@actions/exec')
+const { open, readFile } = require('fs/promises')
 
+// TODO: Make sure that we send the Action version so that old ones can be rejected.
+const CONFIG_FILENAME = 'e2e.toml'
 
-// most @actions toolkit packages have async methods
-async function run() {
+const getConfigString = async () => {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const configBuffer = await readFile(CONFIG_FILENAME)
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
+    return configBuffer.toString()
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(`E2E config file named "${CONFIG_FILENAME}" not found in current working directory.`)
+
+    process.exit(1)
   }
 }
 
-run();
+async function run() {
+  try {
+    const apiKey = core.getInput('api-key', { required: true })
+
+    await getConfigString()
+
+    await exec.exec('zip -r e2e.zip . -x ".git/*" ".github/*"')
+
+    const client = new http.HttpClient('e2e-tool-action')
+
+    const { result } = await client.post('http://host.docker.internal:4444/runs', apiKey)
+
+    console.log('result', result)
+
+    const filehandle = await open('e2e.zip')
+
+    const url = `http://host.docker.internal:4444/stream/${result.runId}`
+
+    await client.sendStream('POST', url, filehandle.createReadStream())
+
+    core.info('E2E run has been started.')
+
+    // TODO: Set up the polling of the run status with the runId.
+  } catch (error) {
+    console.log(error)
+
+    core.setFailed(error.message)
+  }
+}
+
+run()
---
+++ tests/main.test.js
@@ -0,0 +1,3 @@
+// TODO: Set env variable INPUT_API-KEY=temp123 before running the index file.
+// TODO: Mock the fetching of the e2e.toml file.
+// TODO: Throw error if not on Linux.
